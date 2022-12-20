4051e53bb45a42aec040e03cdd7fb3ff2341387b
Jacky Thomas <jakejet64@gmail.com>
2022-12-20 17:26:24 -0500

    set up next two problems

---
+++ RemoveNthFromEnd/removeNthFromEnd.js
@@ -0,0 +1,16 @@
+/*
+Given the head of a linked list,
+remove the nth node from the end
+of the list and return its head.
+
+Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+ */
+
+var removeNthFromEnd = function(head, n) {
+
+};
