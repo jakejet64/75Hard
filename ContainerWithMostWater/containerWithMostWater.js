da870ac13d98f57559a0370ebd530e23d692018b
Jacky Thomas <jakejet64@gmail.com>
2022-12-20 17:20:25 -0500

    Solved ContainerWithMostWater

--- ContainerWithMostWater/containerWithMostWater.js
+++ ContainerWithMostWater/containerWithMostWater.js
@@ -12,5 +12,24 @@ Notice that you may not slant the container.
 */
 
 var maxArea = function(height) {
+  let max = 0;
 
+  let left = 0;
+  let right = height.length - 1;
+
+  while(left !== right) {
+    if(height[left] < height[right]) {
+      max = Math.max(max, (right - left) * height[left]);
+      left++;
+    } else {
+      max = Math.max(max, (right - left) * height[right]);
+      right--;
+    }
+  }
+
+  return max;
 };
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+
+//     [1,8,6,2,5,4,8,3,7]
