6d77ba2284d48ebdae7b2ea74faae3a2d2286d09
Jacky Thomas <jakejet64@gmail.com>
2022-12-20 16:07:30 -0500

    Solved LongestPalindrome

--- LongestPalindrome/longestPalindrome.js
+++ LongestPalindrome/longestPalindrome.js
@@ -4,5 +4,22 @@ palindromic substring in s.
 */
 
 var longestPalindrome = function(s) {
-
+  for(let length = s.length; length > 0; length--) {
+    for(let start = 0; start <= s.length - length; start++) {
+      if(palindrome(s.substr(start, length))) {
+        return s.substr(start, length);
+      }
+    }
+  }
 };
+
+var palindrome = function(s) {
+  for(let i = 0; i < s.length / 2; i++) {
+    if(s.charAt(i) !== s.charAt(s.length - i - 1)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//console.log(longestPalindrome('babad'));
