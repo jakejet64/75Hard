9b1a2a15847d124699915b3f7b3b2de5813be773
Jacky Thomas <jakejet64@gmail.com>
2022-12-22 14:28:09 -0500

    Solved MergeTwoSortedLists

--- MergeTwoSortedLists/mergeTwoSortedLists.js
+++ MergeTwoSortedLists/mergeTwoSortedLists.js
@@ -13,6 +13,34 @@ function ListNode(val, next) {
   this.val = (val===undefined ? 0 : val);
   this.next = (next===undefined ? null : next);
 }
+
 var mergeTwoLists = function(list1, list2) {
 
+  if(!list1) return list2;
+  if(!list2) return list1;
+
+  let newList = new ListNode(Math.min(list1.val, list2.val));
+  (list1.val < list2.val) ? list1 = list1.next : list2 = list2.next;
+
+  let pointer = newList;
+
+  while(list1 && list2) {
+    if(list1.val < list2.val) {
+      pointer.next = new ListNode(list1.val);
+      pointer = pointer.next;
+      list1 = list1.next;
+    } else {
+      pointer.next = new ListNode(list2.val);
+      pointer = pointer.next;
+      list2 = list2.next;
+    }
+  }
+
+  if(list1) {
+    pointer.next = list1;
+  } else {
+    pointer.next = list2;
+  }
+
+  return newList;
 };
