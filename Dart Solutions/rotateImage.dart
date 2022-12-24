23e63cea03317ad4aefa47c3eed21faed71523b5
Jacky Thomas <jakejet64@gmail.com>
2022-12-23 21:49:18 -0500

    Set up next two problems

---
+++ Dart Solutions/rotateImage.dart
@@ -0,0 +1,19 @@
+/*
+You are given an n x n 2D matrix representing
+an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place,
+which means you have to modify the input
+2D matrix directly. DO NOT allocate another
+2D matrix and do the rotation.
+*/
+void main(List<String> arguments) {
+  Solution s = new Solution();
+  print(s.rotate());
+}
+
+class Solution {
+  void rotate(List<List<int>> matrix) {
+
+  }
+}
